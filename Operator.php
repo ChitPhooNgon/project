94c6531e0a15d8d26bf166d4b70de29d25ef3db3
ChitPhooNgon <169056312+ChitPhooNgon@users.noreply.github.com>
2024-05-07 14:35:18 +0630

    Add files via upload

---
+++ Operator.php
@@ -0,0 +1,15 @@
+<?php
+$value1=30;
+// "+" operator
+$value=$value1 + 10;
+echo $value . "<br>";
+//"-" operator
+$value= $value1-10;
+echo $value ."<br>";
+// "*" operator
+$value = $value1 * 3;
+echo $value . "<br>";
+// "/" operator
+$value = $value1 / 3;
+echo $value . "<br>";
+?>
